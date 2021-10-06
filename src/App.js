b752fab20db43c0b9383f4c343eaef90a432be78
Asif-102 <aiqbal517@gmail.com>
2021-10-07 03:20:06 +0600

    created login and register

--- src/App.js
+++ src/App.js
@@ -1,44 +1,116 @@
 import { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.init';
+
+initializeAuthentication();
 
 function App() {
 
-  const [userInfo, setUserInfo] = useState({});
+  const auth = getAuth();
+
+  const [userInfo, setUserInfo] = useState({ isLogin: false });
+  const [registerInfo, setRegisterInfo] = useState({ error: '', success: '' })
+
+  const toggleLogin = (e) => {
+    const logingStatus = { ...userInfo };
+    logingStatus.isLogin = e.target.checked;
+    setUserInfo(logingStatus);
+
+    const clearMessage = {...registerInfo};
+    clearMessage.error = '';
+    clearMessage.success = '';
+    setRegisterInfo(clearMessage); 
+  }
 
   const handleBlur = (e) => {
-    let formData = { ...userInfo };
+    const formData = { ...userInfo };
     formData[e.target.name] = e.target.value;
     setUserInfo(formData);
   }
 
   const handleRegistration = (e) => {
     e.preventDefault();
-    console.log(userInfo);
+
+    const { email, password } = userInfo;
+
+    userInfo.isLogin ? loginUser(email, password) : createNewUser(email, password);
+
+
+  }
+
+  const loginUser = (email, password) => {
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        const regSuccess = { ...registerInfo };
+        regSuccess.error = '';
+        regSuccess.success = 'Login Success';
+        setRegisterInfo(regSuccess);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // console.log(errorMessage, errorCode);
+        const regError = { ...registerInfo };
+        regError.error = errorMessage;
+        regError.success = '';
+        setRegisterInfo(regError);
+      });
+  }
+
+  const createNewUser = (email, password) => {
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // console.log(user);
+        const regSuccess = { ...registerInfo };
+        regSuccess.error = '';
+        regSuccess.success = 'Register Success';
+        setRegisterInfo(regSuccess);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // console.log(errorMessage, errorCode);
+        const regError = { ...registerInfo };
+        regError.error = errorMessage;
+        regError.success = '';
+        setRegisterInfo(regError);
+      });
   }
 
   return (
-    <div className="login-container">
+    <div>
+      <div className="login-container">
 
-      {/* login form */}
+        {/* login form */}
 
-      <form onSubmit={handleRegistration} className="login-form">
-        <h2>Login</h2>
+        <form onSubmit={handleRegistration} className="login-form">
+          <h2>Please {userInfo.isLogin ? 'Login' : 'Register'}</h2>
 
-        <input type="text" name="email" placeholder="Your Email"
-          onBlur={handleBlur}
-          required />
-        <input type="password" name="password" placeholder="Your Password"
-          onBlur={handleBlur}
-          required />
+          <input type="email" name="email" placeholder="Your Email"
+            onBlur={handleBlur}
+            required />
+          <input type="password" name="password" placeholder="Your Password"
+            onBlur={handleBlur}
+            required />
 
-        <div>
-          <input type="checkbox" name="switchForm" id="switchForm" />
-          <label htmlFor="switchForm">Returning User</label>
-        </div>
+          <div>
+            <input type="checkbox" name="switchForm" id="switchForm"
+              onChange={toggleLogin} />
+            <label htmlFor="switchForm">Already Registered?</label>
+          </div>
 
-        <button type="submit">Login</button>
-      </form>
-
+          <button type="submit">{userInfo.isLogin ? 'Login' : 'Register'}</button>
+        </form>
+      </div>
+      <div className="error-message">{registerInfo.error}</div>
+      <div className="success-message">{registerInfo.success}</div>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrCO9lq2AdOfREP2OUS_kO2IK-K015vdk",
+    authDomain: "fir-authentication-12ab0.firebaseapp.com",
+    projectId: "fir-authentication-12ab0",
+    storageBucket: "fir-authentication-12ab0.appspot.com",
+    messagingSenderId: "688529962538",
+    appId: "1:688529962538:web:6c8a130cc91fbcc5143553"
+  };
+
+export default firebaseConfig;
