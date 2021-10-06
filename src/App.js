9f72f2f3081a6d3d747e1467c8970715987048cc
Asif-102 <aiqbal517@gmail.com>
2021-10-07 02:00:47 +0600

    form initialize complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [userInfo, setUserInfo] = useState({});
+
+  const handleBlur = (e) => {
+    let formData = { ...userInfo };
+    formData[e.target.name] = e.target.value;
+    setUserInfo(formData);
+  }
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+
+      {/* login form */}
+
+      <form onSubmit={handleRegistration} className="login-form">
+        <h2>Login</h2>
+
+        <input type="text" name="email" placeholder="Your Email"
+          onBlur={handleBlur}
+          required />
+        <input type="password" name="password" placeholder="Your Password"
+          onBlur={handleBlur}
+          required />
+
+        <div>
+          <input type="checkbox" name="switchForm" id="switchForm" />
+          <label htmlFor="switchForm">Returning User</label>
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+
     </div>
   );
 }
